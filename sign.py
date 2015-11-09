1525c69d5217a9952dc4606208e6b3c7007d849d
Rafael Akchurin <rafael.akchurin@diladele.com>
2015-11-09 01:21:26 +0100

    updated version

--- sign.py
+++ sign.py
@@ -6,7 +6,7 @@ def sign(file, pfx):
     
     cmd = " ".join(
         [
-            "c:/Program Files (x86)/Windows Kits/8.0/bin/x64/signtool.exe", 
+            "c:/Program Files (x86)/Windows Kits/8.1/bin/x64/signtool.exe", 
             "sign", 
             "/ph", 
             "/v", 
