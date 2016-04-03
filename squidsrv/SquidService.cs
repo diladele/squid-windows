4d9f209b6e962ef13c8760d4098c1673077a6ef7
Diladele <support@diladele.com>
2016-04-03 23:44:17 +0200

    Switching to 3.5.16

--- squidsrv/SquidService.cs
+++ squidsrv/SquidService.cs
@@ -98,7 +98,7 @@ namespace Diladele.Squid.Service
                     var remoteVersionFile = PredefinedPaths.InstallationFolder + @"\var\log\squid.version";
 
                     var req = (HttpWebRequest)WebRequest.Create("https://defs.diladele.com/squid/version/windows");
-                    req.UserAgent = "Squid3.5.14/" + Environment.OSVersion.VersionString + "/x64 (win;0-0-0-0)";
+                    req.UserAgent = "Squid3.5.16/" + Environment.OSVersion.VersionString + "/x64 (win;0-0-0-0)";
                     req.Headers.Add("Authorization", "Token 0000000000000000");
 
                     WebResponse resp = req.GetResponse();
